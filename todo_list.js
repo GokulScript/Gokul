a479a9f4a0bd5dc4e88b5f3113f1543dde80d316
GokulScript <155992102+GokulScript@users.noreply.github.com>
2024-01-16 10:32:54 +0530

    Update todo_list.js

--- todo_list.js
+++ todo_list.js
@@ -1,37 +1,132 @@
-const inputBox=document.getElementById("input_value");
-const list_container=document.getElementById("list_container");
+
+
+let UL = document.getElementById("listContainer"); 
+
+let MyDiv = document.createElement("div");
+MyDiv.classList.add("TaskContainer");
 
 
 
-function AddTask(){
-    if(inputBox.value===""){
-        alert("Enter your task");
+
+
+
+
+
+function NewTask() {
+    //let unchecked = '<i class="fa-solid fa-square fa-2xl"></i>';
+    let unchecked=document.createElement('i');
+    unchecked.classList.add("fa-solid", "fa-square", "fa-2xl");
+
+
+   
+    unchecked.addEventListener('click',handle);
+
+ //   let checked = '<i class="fa-solid fa-square-check fa-2xl "></i>';
+    let checked=document.createElement('i');
+    checked.classList.add("fa-solid","fa-square-check","fa-2xl");
+
+   // let X = '<i class="fa-solid fa-square-xmark fa-2xl"></i>';
+   let X=document.createElement('i');
+   X.classList.add("fa-solid","fa-square-xmark","fa-2xl");
+   X.addEventListener('click',deletes);
+
+   
+    let MyDiv = document.createElement("div");
+    MyDiv.classList.add("TaskContainer");
+
+    let input = document.getElementById('inputText');
+    /*let UL = document.getElementById("listContainer"); */
+
+
+
+
+    
+
+
+    if (input.value === "") {
+        alert("first enter");
+    } else {
+        let textSpan = document.createElement("span");
+        textSpan.classList.add("hello");
+        textSpan.textContent = input.value;
+
+
+
+        MyDiv.appendChild(unchecked);  // Append the unchecked icon
+        MyDiv.appendChild(textSpan);
+        MyDiv.appendChild(X);
+
+      // MyDiv.append(textSpan);
+
+       //MyDiv.innerHTML = unchecked.outerHTML + MyDiv.innerHTML + X.outerHTML;
+  
+        UL.appendChild(MyDiv);
+
+        
+       
+   
+        input.value = "";
+        
+
+        saveData();
+
+       
+
     }
-    else{
-        let li=document.createElement("li");
-        li.innerHTML=inputBox.value;
-        list_container.appendChild(li)
-        let img=document.createElement("img")
-        img.src="/images/x.jpg";
-        li.appendChild(img);
-    }
-    inputBox.value="";
+}
+
+function handle(){
+    this.classList.toggle("fa-square");
+    this.classList.toggle("fa-square-check");
+    console.log("the icon function is executing")
     saveData();
+
+
 }
-list_container.addEventListener("click",function(e){
-    if (e.target.tagName==="LI"){
-        e.target.classList.toggle("checked");
+var element=document.getElementsByClassName("fa-solid fa-square fa-2xl");
+for(var i=0;i<element.length;i++){
+    element[i].addEventListener('click',handle);
+}
+
+function deletes(){
+        this.parentElement.remove();
         saveData();
-    }
-    else if (e.target.tagName==="IMG"){
-        e.target.parentElement.remove();
-        saveData();
-    }
-},false);
-function  saveData(){
-    localStorage.setItem("data",list_container.innerHTML);
+
 }
-function showTAsk(){
-    list_container.innerHTML=localStorage.getItem("data");
+
+
+function saveData(){
+    localStorage.setItem("data",UL.innerHTML);
 }
-showTAsk();
+function showTask(){
+
+
+  
+    UL.innerHTML=localStorage.getItem("data") || "";
+
+
+
+    var element=document.getElementsByClassName("fa-solid fa-square fa-2xl");
+    for(var i=0;i<element.length;i++){
+        element[i].addEventListener('click',handle);
+    }
+
+    var deleteIcon=document.getElementsByClassName("fa-solid fa-square-xmark fa-2xl");
+    for(var j=0;i<deleteIcon.length;j++){
+        deleteIcon[j].addEventListener('click',deletes);
+    }
+
+    
+
+
+    
+
+
+
+}
+
+
+showTask();
+
+
+
